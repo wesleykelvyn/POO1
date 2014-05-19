8a44da916d21446c9d82e7d11ab714645b25b15a
Rodrigo Vilar <rvilar@gmail.com>
2014-05-19 11:34:11 -0300

    Exemplo de Banco e Agencia, com teste

---
+++ Agencia.java
@@ -0,0 +1,23 @@
+public class Agencia {
+    
+    private int numero;
+    private Banco banco;
+    
+    
+    public void setNumero(int numero) {
+        this.numero = numero;
+    }
+
+    public int getNumero() {
+        return numero;
+    }
+
+    public void setBanco(Banco banco) {
+        this.banco = banco;
+    }
+
+    public Banco getBanco() {
+        return banco;
+    }
+
+}
