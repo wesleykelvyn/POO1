712463e8504cfe41877cbdbc6944821a91fc98e4
Rodrigo Vilar <rvilar@gmail.com>
2014-05-20 11:36:37 -0300

    Passo 4

--- Agencia.java
+++ Agencia.java
@@ -5,16 +5,15 @@ public class Agencia {
     
     private int numero;
     private Banco banco;
-    private List<Conta> contas = new ArrayList<Conta>();
-    private List<Poupanca> poupancas = new ArrayList<Poupanca>();
+    private List<ContaAbstrata> contas = new ArrayList<ContaAbstrata>();
 
-    public void cadastrarConta(Conta conta) {
+    public void cadastrarConta(ContaAbstrata conta) {
         contas.add(conta);
     }
     
-    public Conta pesquisarContaPorNumero(int numero) {
+    public ContaAbstrata pesquisarContaPorNumero(int numero) {
         
-        for (Conta conta : contas) {
+        for (ContaAbstrata conta : contas) {
             
             if (conta.getNumero() == numero) {
                 return conta;
@@ -22,22 +21,6 @@ public class Agencia {
         }
         
         throw new RuntimeException("Conta não encontrada: " + numero);
-    }
-
-    public void cadastrarPoupanca(Poupanca poupanca) {
-        poupancas.add(poupanca);
-    }
-    
-    public Poupanca pesquisarPoupancaPorNumero(int numero) {
-        
-        for (Poupanca poupanca : poupancas) {
-            
-            if (poupanca.getNumero() == numero) {
-                return poupanca;
-            }
-        }
-        
-        throw new RuntimeException("Poupanca não encontrada: " + numero);
     }
 
     public void setNumero(int numero) {
