47861feb4601deca9de6fb66f9120d6f5ec9aa7e
Rodrigo Vilar <rvilar@gmail.com>
2014-05-20 11:39:46 -0300

    Passo 6

--- Agencia.java
+++ Agencia.java
@@ -5,15 +5,15 @@ public class Agencia {
     
     private int numero;
     private Banco banco;
-    private List<ContaAbstrata> contas = new ArrayList<ContaAbstrata>();
+    private List<ContaIF> contas = new ArrayList<ContaIF>();
 
-    public void cadastrarConta(ContaAbstrata conta) {
+    public void cadastrarConta(ContaIF conta) {
         contas.add(conta);
     }
     
-    public ContaAbstrata pesquisarContaPorNumero(int numero) {
+    public ContaIF pesquisarContaPorNumero(int numero) {
         
-        for (ContaAbstrata conta : contas) {
+        for (ContaIF conta : contas) {
             
             if (conta.getNumero() == numero) {
                 return conta;
