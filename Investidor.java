23887382ae9b7cdcc94b84b2f6c94c6679b1ea31
Aluno <me@me.com>
2014-05-26 11:43:32 -0300

    passo3

---
+++ Investidor.java
@@ -0,0 +1,20 @@
+
+public class Investidor extends Cliente {
+	private String clienteMaster;
+	
+	public Investidor (String clienteMaster){
+		this.clienteMaster=clienteMaster;
+	}
+	
+
+	public String getClienteMaster() {
+		return clienteMaster;
+	}
+
+	public void setClienteMaster(String clienteMaster) {
+		this.clienteMaster = clienteMaster;
+	}
+	
+	}
+
+
