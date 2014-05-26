98047a40065bf92316707f8cbace1ec00626be0b
Aluno <me@me.com>
2014-05-26 10:56:09 -0300

    Criando projeto do eclipse e adicionando Funcionario

--- Banco.java
+++ Banco.java
@@ -1,46 +1,58 @@
 import java.util.*;
 
-
 public class Banco {
-    
-    
-    private String nome;
-    private int numero;
-    private List<Agencia> agencias = new ArrayList<Agencia>();
-    
-    public void cadastrarAgencia(Agencia agencia) {
-        agencias.add(agencia);
-    }
-    
-    public Agencia pesquisarAgenciaPorNumero(int numero) {
-        
-        for (Agencia agencia : agencias) {
-            
-            if (agencia.getNumero() == numero) {
-                return agencia;
-            }
-        }
-        
-        throw new RuntimeException("Agencia não encontrada: " + numero);
-    }
-    
-    public void setNome(String nome) {
-        this.nome = nome;
-    }
 
-    public String getNome() {
-        return nome;
-    }
+	private String nome;
+	private int numero;
+	private List<Agencia> agencias = new ArrayList<Agencia>();
+	private List<Funcionario> funcionarios = new ArrayList<Funcionario>();
 
-    public void setNumero(int numero) {
-        this.numero = numero;
-    }
+	public void cadastrarFuncionario(Funcionario funcionario) {
+		funcionarios.add(funcionario);
+	}
 
-    public int getNumero() {
-        return numero;
-    }
-    
-    
-    
+	public Funcionario pesquisarFuncionarioPorCPF(String cpf) {
+
+		for (Funcionario funcionario : funcionarios) {
+
+			if (funcionario.getCpf().equals(cpf)) {
+				return funcionario;
+			}
+		}
+
+		throw new RuntimeException("Funcionario n�o encontrado: " + cpf);
+	}
+
+	public void cadastrarAgencia(Agencia agencia) {
+		agencias.add(agencia);
+	}
+
+	public Agencia pesquisarAgenciaPorNumero(int numero) {
+
+		for (Agencia agencia : agencias) {
+
+			if (agencia.getNumero() == numero) {
+				return agencia;
+			}
+		}
+
+		throw new RuntimeException("Agencia não encontrada: " + numero);
+	}
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+
+	public String getNome() {
+		return nome;
+	}
+
+	public void setNumero(int numero) {
+		this.numero = numero;
+	}
+
+	public int getNumero() {
+		return numero;
+	}
 
 }
---
+++ Funcionario.java
@@ -0,0 +1,31 @@
+public class Funcionario {
+
+	private String nome;
+	private String cpf;
+	private int matricula;
+
+	public String getNome() {
+		return nome;
+	}
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+
+	public String getCpf() {
+		return cpf;
+	}
+
+	public void setCpf(String cpf) {
+		this.cpf = cpf;
+	}
+
+	public int getMatricula() {
+		return matricula;
+	}
+
+	public void setMatricula(int matricula) {
+		this.matricula = matricula;
+	}
+
+}
